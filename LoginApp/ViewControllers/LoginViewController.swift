47b00d2e58d903516f67dbfa3994536496c26f81
Eugene <zgainer@gmail.com>
2022-11-08 13:31:23 +0300

    MVC started

---
+++ LoginApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Eugene on 4.11.22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet var usernameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    private let username = "User"
+    private let password = "12345"
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+
+        welcomeVC.username = username
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        usernameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    @IBAction func forgotButtonTapped(_ sender: UIButton) {
+        sender.tag == 0
+            ? showAlert(title: "Reminder", message: "Your username is \(username)")
+            : showAlert(title: "Reminder", message: "Your password is \(password)")
+    }
+    
+    @IBAction func loginButtonTapped() {
+        guard usernameTF.text == username, passwordTF.text == password else {
+            showAlert(title: "Error",
+                      message: "Wrong username or password!",
+                      textField: passwordTF)
+            
+            return
+        }
+        
+//        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
+    }
+}
+
+extension LoginViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+
+        let ok = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+
+        alert.addAction(ok)
+        
+        present(alert, animated: true)
+    }
+    //        let fill = UIAlertAction(title: "Fill",
+    //                                 style: .destructive) { _ in
+    //            self.passwordTF.text = self.password
+    //        }
+    //
+    //        alertMessage.addAction(fill)
+}
